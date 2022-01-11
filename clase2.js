f446bf2ff02deb0d770bd6fc8bb05d03334ed41e
Erika Sandoval <erikasandoval-09@hotmail.com>
2022-01-11 20:03:29 -0300

    Ejercicio Clase 2

---
+++ clase2.js
@@ -0,0 +1,14 @@
+//CONSIGNA: Crear una funcion principal en la cual se declaren las edades de 3 personas. 
+//Dentro de la funcion creada, llamar a otra funcion que reciba 4 parametros (las 3 declaradas anteriormente y un parametro con valor numerico por default) y retorne el promedio de los 4 numeros. 
+//En la primer funcion creada, mostrar el promedio calculado en consola.
+
+
+let edades2 = (edad1, edad2, edad3, edad4 = 5) => {
+    return promedio = (edad1 + edad2 + edad3 + edad4) / 4;
+}
+let edades = (edad1, edad2, edad3) => {
+    console.log(edades2(edad1, edad2, edad3));
+}
+edades(10, 6, 7);
+
+
