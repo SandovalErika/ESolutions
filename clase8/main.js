a77e69f3d0e95ef293332a8fe2358890be15cffa
Erika Sandoval <erikasandoval-09@hotmail.com>
2022-01-25 07:45:18 -0300

    Cambios y mejoras Desafio 3

--- clase8/main.js
+++ clase8/main.js
@@ -2,38 +2,40 @@
 
 // Deben generar fetch en paralelo y cando se hayan cargado los nombres, cargar la película y luego ir a la siguiente. En caso de que ocurriese un error en alguno de los fetch deben "solucionarlo" de alguna forma, pero la película y el "listado de personajes" no se debe dejar de mostrar. Por otro lado, si el personaje ya existe, no se debe volver a pedir en un fetch para la siguiente película, sino extraerlo de un Set o arrary.
 
-let loadJson = async () => {
-  return fetch("https://swapi.dev/api/films")
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-      const list = document.querySelector("ul");
-      for (let i = 0; i < json.results.length; i++) {
-        const movie = document.createElement("li");
-        movie.innerHTML = `<h3>${json.results[i].title}</h3>`;
+const loadJson = async () => {
+  try {
+    const response = await fetch("https://swapi.dev/api/films");
+    const json = await response.json();
+    const list = document.querySelector("ul");
 
-        movie.innerHTML += `<p>Episodio: ${json.results[i].episode_id}</p>`;
-        movie.innerHTML += `<p>${json.results[i].opening_crawl}</p>`;
-        let castList = "<ul>";
-        json.results[i].characters.forEach((character) => {
-          let urlSplit = character.split("/");
-          let id = urlSplit[5];
-          console.log(id);
-          fetch(character)
-            .then((response) => response.json())
-            .then((json) => {
-              castList += `<li>${json.name}</li>`;
-              movie.innerHTML += castList;
-            });
-          console.log(character);
-        });
+    for (let i = 0; i < json.results.length; i++) {
+      const movie = document.createElement("li");
 
-        list.appendChild(movie);
-      }
-    })
-    .catch((error) => {
-      error.name = "Error";
-      error.message = "Error al cargar la página";
-      console.log(error);
-    });
+      movie.innerHTML = `<h2>${json.results[i].title}</h2>`;
+      movie.innerHTML += `<h3>Episode ${json.results[i].episode_id}</h3>`;
+      movie.innerHTML += `<p>${json.results[i].opening_crawl}</p>`;
+
+      let castList = "<ul>";
+      let url = json.results[i].characters.map(async (url) => {
+        return `<li>${await urlCharacter(url)}</li>`;
+      });
+
+      let dataUrl = await Promise.all(url);
+      console.log(dataUrl);
+
+      castList += dataUrl.join("");
+      console.log(castList);
+
+      movie.innerHTML += `Cast: ${castList}`;
+      list.appendChild(movie);
+    }
+  } catch (error) {
+    console.log(error.title);
+  }
+};
+
+const urlCharacter = async (url) => {
+  const respCharacter = await fetch(url);
+  const dato = await respCharacter.json();
+  return dato.name;
 };
