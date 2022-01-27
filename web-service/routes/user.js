ba5c40b4e8b9fbe3aa873c35ee818ece3a59d7cc
Erika Sandoval <erikasandoval-09@hotmail.com>
2022-01-26 22:22:57 -0300

    Desafio Web-Service

---
+++ web-service/routes/user.js
@@ -0,0 +1,157 @@
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await req.context.models.users.find();
+    return res.send(users);
+  } catch (e) {
+    if (res.status(404)) {
+      return res.status(404).json({
+        message: "USUARIO NO ENCONTRADO",
+      });
+    } else if (res.status(400)) {
+      return res.status(400).json({
+        message: "ERROR DE PETICION",
+      });
+    } else if (res.status(403)) {
+      return res.status(403).json({
+        message: "ACCESO NO PERMITIDO",
+      });
+    } else if (res.status(408)) {
+      return res.status(408).json({
+        message: "TIMED OUT",
+      });
+    } else {
+      return res.status(500).json({
+        message: "ERROR DESCONOCIDO",
+      });
+    }
+  }
+});
+
+router.get("/:userId", async (req, res) => {
+  try {
+    const user = await req.context.models.users.findById(req.params.userId);
+    res.send(user);
+  } catch (e) {
+    if (res.status(404)) {
+      return res.status(404).json({
+        message: "USUARIO NO ENCONTRADO",
+      });
+    } else if (res.status(400)) {
+      return res.status(400).json({
+        message: "ERROR DE PETICION",
+      });
+    } else if (res.status(403)) {
+      return res.status(403).json({
+        message: "ACCESO NO PERMITIDO",
+      });
+    } else if (res.status(408)) {
+      return res.status(408).json({
+        message: "TIMED OUT",
+      });
+    } else {
+      return res.status(500).json({
+        message: "ERROR DESCONOCIDO",
+      });
+    }
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await req.context.models.users.create({
+      username: req.body.username,
+    });
+    return res.send(user);
+  } catch (e) {
+    if (res.status(404)) {
+      return res.status(404).json({
+        message: "USUARIO NO ENCONTRADO",
+      });
+    } else if (res.status(400)) {
+      return res.status(400).json({
+        message: "ERROR DE PETICION",
+      });
+    } else if (res.status(403)) {
+      return res.status(403).json({
+        message: "ACCESO NO PERMITIDO",
+      });
+    } else if (res.status(408)) {
+      return res.status(408).json({
+        message: "TIMED OUT",
+      });
+    } else {
+      return res.status(500).json({
+        message: "ERROR DESCONOCIDO",
+      });
+    }
+  }
+});
+
+router.put("/:userId", async (req, res) => {
+  try {
+    const user = await req.context.models.users.findById(req.params.userId);
+    if (user) {
+      user.username = req.body.username;
+      await user.save();
+    }
+    return res.send(user);
+  } catch (e) {
+    if (res.status(404)) {
+      return res.status(404).json({
+        message: "USUARIO NO ENCONTRADO",
+      });
+    } else if (res.status(400)) {
+      return res.status(400).json({
+        message: "ERROR DE PETICION",
+      });
+    } else if (res.status(403)) {
+      return res.status(403).json({
+        message: "ACCESO NO PERMITIDO",
+      });
+    } else if (res.status(408)) {
+      return res.status(408).json({
+        message: "TIMED OUT",
+      });
+    } else {
+      return res.status(500).json({
+        message: "ERROR DESCONOCIDO",
+      });
+    }
+  }
+});
+
+router.delete("/:userId", async (req, res) => {
+  try {
+    const user = await req.context.models.users.findById(req.params.userId);
+    if (user) {
+      await user.remove();
+    }
+    return res.send(user);
+  } catch (e) {
+    if (res.status(404)) {
+      return res.status(404).json({
+        message: "USUARIO NO ENCONTRADO",
+      });
+    } else if (res.status(400)) {
+      return res.status(400).json({
+        message: "ERROR DE PETICION",
+      });
+    } else if (res.status(403)) {
+      return res.status(403).json({
+        message: "ACCESO NO PERMITIDO",
+      });
+    } else if (res.status(408)) {
+      return res.status(408).json({
+        message: "TIMED OUT",
+      });
+    } else {
+      return res.status(500).json({
+        message: "ERROR DESCONOCIDO",
+      });
+    }
+  }
+});
+
+module.exports = router;
